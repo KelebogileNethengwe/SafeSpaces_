c812f5d600cc61c1a7dda7202722f5b487675845
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-08-06 05:25:09 +0200

    pages

--- signInsignUp.js
+++ signInsignUp.js
@@ -9,3 +9,40 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+// Global variable to store selected locations
+let selectedLocations = [];
+
+function addLocation() {
+  const locationSelect = document.getElementById('location-select');
+  const selectedLocation = locationSelect.value;
+
+  if (!selectedLocation) {
+    alert('Please select a location.');
+    return;
+  }
+
+  // Check if the location is already selected
+  if (selectedLocations.includes(selectedLocation)) {
+    alert('This location is already selected.');
+    return;
+  }
+
+  // Add the selected location to the list
+  selectedLocations.push(selectedLocation);
+  updateSelectedLocationsList();
+}
+
+function updateSelectedLocationsList() {
+  const selectedLocationsList = document.getElementById('selected-locations-list');
+  selectedLocationsList.innerHTML = '';
+
+  // Update the list of selected locations
+  for (const location of selectedLocations) {
+    const listItem = document.createElement('li');
+    listItem.textContent = location;
+    selectedLocationsList.appendChild(listItem);
+  }
+}
+
